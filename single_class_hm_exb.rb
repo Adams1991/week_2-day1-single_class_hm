dc52aad8e9d3b0adb33ff4fd4f22d9e86d9baf85
Adams1991 <adams121@hotmail.co.uk>
2018-05-28 16:44:48 +0100

    Ex B gets done. Not refactored

--- single_class_hm_exb.rb
+++ single_class_hm_exb.rb
@@ -1,3 +1,21 @@
 class Team
 
+  def initialize (team_name, players, coach)
+    @name = team_name
+    @players = players
+    @coach = coach
+  end
+
+  def get_team_name()
+    return @name
+  end
+
+  def get_players()
+    return @players
+  end
+
+  def get_coach()
+    return @coach
+  end
+
 end
--- spec_exb/single_class_hm_exb_spec.rb
+++ spec_exb/single_class_hm_exb_spec.rb
@@ -3,4 +3,19 @@ require_relative("../single_class_hm_exb")
 
 class TeamTest < MiniTest::Test
 
+  def test_get_team_name()
+    student = Team.new("The Falcons", ["Bob", "Bill"], "John")
+    assert_equal("The Falcons", student.get_team_name())
+  end
+
+  def test_get_players()
+    student = Team.new("The Falcons", ["Bob", "Bill"], "John")
+    assert_equal(["Bob", "Bill"], student.get_players())
+  end
+
+  def test_get_coach()
+      student  = Team.new("The Falcons",["Bob", "Bill"], "John")
+      assert_equal("John",  student.get_coach())
+  end
+
 end
