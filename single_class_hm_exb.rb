923d730b10361823ead0a33a6b0bdb6118fe648a
Adams1991 <adams121@hotmail.co.uk>
2018-05-28 17:50:51 +0100

    redaction complete but 1 failing test

--- single_class_hm_exb.rb
+++ single_class_hm_exb.rb
@@ -1,33 +1,37 @@
 class Team
 
+attr_accessor(:team_name, :players, :coach)
+
   def initialize (team_name, players, coach)
     @name = team_name
     @players = players
     @coach = coach
   end
 
-  def get_team_name()
-    return @name
-  end
 
-  def get_players()
-    return @players
-  end
-
-  def get_coach()
-    return @coach
-  end
-
-  def set_team_name(name)
-    @name = name
-  end
-
-  def set_players(players)
-    @players = players
-  end
-
-  def set_coach(coach)
-    @coach = coach
-  end
 
 end
+
+# def get_team_name()
+#   return @name
+# end
+#
+# def get_players()
+#   return @players
+# end
+#
+# def get_coach()
+#   return @coach
+# end
+#
+# def set_team_name(name)
+#   @name = name
+# end
+#
+# def set_players(players)
+#   @players = players
+# end
+#
+# def set_coach(coach)
+#   @coach = coach
+# end
--- spec_exb/single_class_hm_exb_spec.rb
+++ spec_exb/single_class_hm_exb_spec.rb
@@ -5,35 +5,35 @@ class TeamTest < MiniTest::Test
 
   def test_get_team_name()
     team = Team.new("The Falcons", ["Bob", "Bill"], "John")
-    assert_equal("The Falcons", team.get_team_name())
+    assert_equal("The Falcons", team.team_name())
   end
 
   def test_get_players()
     team = Team.new("The Falcons", ["Bob", "Bill"], "John")
-    assert_equal(["Bob", "Bill"], team.get_players())
+    assert_equal(["Bob", "Bill"], team.players())
   end
 
   def test_get_coach()
       team  = Team.new("The Falcons",["Bob", "Bill"], "John")
-      assert_equal("John",  team.get_coach())
+      assert_equal("John",  team.coach())
   end
 
   def test_set_team_name()
       team  = Team.new("The Falcons",["Bob", "Bill"], "John")
-      team.set_team_name("Tony")
-      assert_equal("Tony",  team.get_team_name())
+      team.team_name = "Tony"
+      assert_equal("Tony",  team.team_name())
   end
 
   def test_set_players()
       team  = Team.new("The Falcons",["Bob", "Bill"], "John")
-      team.set_players(["Bobby", "Billy"])
-      assert_equal(["Bobby", "Billy"],  team.get_players())
+      team.players = ["Bobby", "Billy"]
+      assert_equal(["Bobby", "Billy"],  team.players())
   end
 
   def test_set_coach()
       team  = Team.new("The Falcons",["Bob", "Bill"], "John")
-      team.set_coach("Dave")
-      assert_equal("Dave",  team.get_coach())
+      team.coach = "Dave"
+      assert_equal("Dave",  team.coach())
   end
 
 end
